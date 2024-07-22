b09495c36a234137adef6e699d9803970b359ac6
esraaKamel1195 <esraa.kamel1811@gmail.com>
2024-07-22 13:52:48 +0200

    Authentication form & routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () =>
+      import('./authentication/authentication.route').then((m) => m.authRoute),
+  },
+  {
     path: '',
     loadComponent: () =>
       import('./to-do-list/to-do-list.component').then(
@@ -13,6 +18,13 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./to-do-list/to-do-list.component').then(
         (c) => c.ToDoListComponent
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./not-found-page/not-found-page.component').then(
+        (c) => c.NotFoundPageComponent
       ),
   },
 ];
