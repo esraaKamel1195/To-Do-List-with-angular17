0bd89ee4337958ab73af16161b45f030f45652f2
Esraa_kamel <esraa.kamel1811@gmail.com>
2025-06-12 19:12:23 +0200

    create new component to add item

---
+++ src/app/add-item/add-item.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialog,
+  MatDialogRef,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogTitle,
+  MatDialogContent,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-add-item',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogTitle,
+    MatDialogContent,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
+  templateUrl: './add-item.component.html',
+  styleUrl: './add-item.component.scss',
+})
+export class AddItemComponent {
+  constructor(public dialogRef: MatDialogRef<AddItemComponent>) {}
+}
